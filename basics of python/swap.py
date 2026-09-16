345451581c237809ba121ea9540400c02bc4f5f1
sundramgupta2509 <sundramgupta053@gmail.com>
2026-09-16 21:31:45 +0530

    swap

---
+++ basics of python/swap.py
@@ -0,0 +1,8 @@
+a= float(input("Enter first number: "))
+b= float(input("Enter second number: "))
+temp = a 
+a =b 
+b =temp 
+print("After swapping: ")
+print("First number: ", a)
+print("Second number: ", b)
